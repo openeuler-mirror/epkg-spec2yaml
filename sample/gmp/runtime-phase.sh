b07a3a5c40678151a8f8f643fd9d94aa4baf646b
邱堂珂 <q_qiutangke@163.com>
2022-10-18 08:30:48 +0000

    http-parser
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

---
+++ sample/gmp/runtime-phase.sh
@@ -0,0 +1,19 @@
+#!/usr/bash
+
+post_c++() {
+-p /sbin/ldconfig
+}
+
+postun_c++() {
+-p /sbin/ldconfig
+}
+
+post() {
+-p /sbin/ldconfig
+}
+
+postun() {
+-p /sbin/ldconfig
+
+}
+
---
+++ sample/grep/phase.sh
@@ -0,0 +1,29 @@
+#!/usr/bash
+
+prep() {
+%autosetup -n %{name}-%{version} -p1
+
+}
+
+build() {
+%configure --disable-silent-rules \
+CPPFLAGS="-I%{_includedir}/pcre2" CFLAGS="$RPM_OPT_FLAGS -fsigned-char"
+%make_build
+
+}
+
+install() {
+%make_install
+rm -f $RPM_BUILD_ROOT%{_infodir}/dir
+mkdir -p $RPM_BUILD_ROOT%{_sysconfdir}/profile.d
+
+install -pm 644 %{SOURCE1} %{SOURCE2} $RPM_BUILD_ROOT%{_sysconfdir}/profile.d
+install -Dpm 755 %{SOURCE3} $RPM_BUILD_ROOT%{_libexecdir}/grepconf.sh
+
+}
+
+check() {
+make check
+
+}
+
