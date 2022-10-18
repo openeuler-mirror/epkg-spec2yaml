91af64b88f4d1bcf3a4c119751e9aa20c70777bc
邱堂珂 <q_qiutangke@163.com>
2022-10-18 08:30:25 +0000

    glib2
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

---
+++ sample/gdb/phase.sh
@@ -0,0 +1,171 @@
+#!/usr/bash
+
+prep() {
+%autosetup -n %{gdb_src} -p1
+
+(cd gdb;rm -fv $(perl -pe 's/\\\n/ /' <Makefile.in|sed -n 's/^YYFILES = //p'))
+
+find -name "*.info*"|xargs rm -f
+
+find -name "*.orig" | xargs rm -f
+
+cat > gdb/version.in << _FOO
+openEuler %{version}-%{release}
+_FOO
+
+rm -f libdecnumber/gstdint.h
+rm -f bfd/doc/*.info
+rm -f bfd/doc/*.info-*
+rm -f gdb/doc/*.info
+rm -f gdb/doc/*.info-*
+mv -f readline/readline/doc readline-doc
+rm -rf readline/readline/*
+mv -f readline-doc readline/readline/doc
+rm -rf zlib texinfo
+
+}
+
+build() {
+rm -rf %{buildroot}
+test -e %{_libdir}/%{librpmso}
+
+mkdir %{gdb_build}
+cd %{gdb_build}
+
+export CFLAGS="$RPM_OPT_FLAGS -DDNF_DEBUGINFO_INSTALL -fPIC"
+export LDFLAGS="%{?__global_ldflags}"
+export CXXFLAGS="$CFLAGS"
+
+../configure							\
+	--prefix=%{_prefix}					\
+	--libdir=%{_libdir}					\
+	--sysconfdir=%{_sysconfdir}				\
+	--mandir=%{_mandir}					\
+	--infodir=%{_infodir}					\
+	--with-system-gdbinit=%{_sysconfdir}/gdbinit		\
+	--with-gdb-datadir=%{_datadir}/gdb			\
+	--enable-gdb-build-warnings=,-Wno-unused		\
+	--enable-build-with-cxx					\
+	--enable-werror						\
+	--with-separate-debug-dir=/usr/lib/debug		\
+	--disable-sim						\
+	--disable-rpath						\
+	--without-stage1-ldflags				\
+	--disable-libmcheck					\
+	--with-babeltrace					\
+	--with-guile						\
+	--with-system-readline					\
+	--with-expat						\
+	--without-libexpat-prefix				\
+	--enable-tui						\
+	--with-python=%{__python3}				\
+	--with-rpm=%{librpmso}			\
+	--with-lzma						\
+	--without-libunwind					\
+	--enable-64-bit-bfd					\
+%ifnarch riscv64
+--enable-inprocess-agent				\
+%endif
+--with-system-zlib					\
+%ifarch %{ix86} x86_64
+--with-intel-pt						\
+%else
+--without-intel-pt					\
+%endif
+--with-mpfr						\
+	--with-auto-load-dir='$debugdir:$datadir/auto-load'	\
+	--with-auto-load-safe-path='$debugdir:$datadir/auto-load'	\
+	--enable-targets=aarch64-linux-gnu %{_target_platform}
+
+make %{?_smp_mflags} CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" V=1 maybe-configure-gdb
+perl -i.relocatable -pe 's/^(D\[".*_RELOCATABLE"\]=" )1(")$/${1}0$2/' gdb/config.status
+
+make %{?_smp_mflags} CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" V=1
+
+! grep '_RELOCATABLE.*1' gdb/config.h
+
+cd ..
+
+cd %{gdb_build}
+make %{?_smp_mflags} \
+     -C gdb/doc {gdb,annotate}{.info,/index.html,.pdf} MAKEHTMLFLAGS=--no-split MAKEINFOFLAGS=--no-split V=1
+
+cp $RPM_BUILD_DIR/%{gdb_src}/gdb/NEWS $RPM_BUILD_DIR/%{gdb_src}
+
+}
+
+check() {
+
+}
+
+install() {
+cd %{gdb_build}
+rm -rf $RPM_BUILD_ROOT
+
+make %{?_smp_mflags} install DESTDIR=$RPM_BUILD_ROOT
+
+mkdir -p $RPM_BUILD_ROOT%{_prefix}/libexec
+mv -f $RPM_BUILD_ROOT%{_bindir}/gdb $RPM_BUILD_ROOT%{_prefix}/libexec/gdb
+ln -s -r $RPM_BUILD_ROOT%{_prefix}/libexec/gdb  $RPM_BUILD_ROOT%{_bindir}/gdb
+
+mkdir -p $RPM_BUILD_ROOT%{_sysconfdir}/gdbinit.d
+touch -r %{SOURCE2} $RPM_BUILD_ROOT%{_sysconfdir}/gdbinit.d
+sed 's#%%{_sysconfdir}#%{_sysconfdir}#g' <%{SOURCE2} >$RPM_BUILD_ROOT%{_sysconfdir}/gdbinit
+touch -r %{SOURCE2} $RPM_BUILD_ROOT%{_sysconfdir}/gdbinit
+
+for i in `find $RPM_BUILD_ROOT%{_datadir}/gdb/python/gdb -name "*.py"`
+do
+touch -r $RPM_BUILD_DIR/%{gdb_src}/gdb/ChangeLog $i
+done
+
+%if 0%{?_enable_debug_packages:1}
+mkdir -p $RPM_BUILD_ROOT/usr/lib/debug%{_bindir}
+cp -p ./gdb/gdb-gdb.py $RPM_BUILD_ROOT/usr/lib/debug%{_bindir}/
+for pyo in "" "-O";do
+%{__python3} $pyo -c 'import compileall, re, sys; sys.exit (not compileall.compile_dir("'"$RPM_BUILD_ROOT/usr/lib/debug%{_bindir}"'", 1, "'"/usr/lib/debug%{_bindir}"'"))'
+done
+%endif # 0%{?_enable_debug_packages:1}
+
+for i in $(echo bin lib $(basename %{_libdir}) sbin|tr ' ' '\n'|sort -u);do
+mkdir -p $RPM_BUILD_ROOT%{_datadir}/gdb/auto-load/%{_prefix}/$i
+ln -s $(echo %{_prefix}|sed 's#^/*##')/$i \
+        $RPM_BUILD_ROOT%{_datadir}/gdb/auto-load/$i
+done
+for i in `find $RPM_BUILD_ROOT%{_datadir}/gdb -name "*.py"`; do
+touch -r $RPM_BUILD_DIR/%{gdb_src}/gdb/ChangeLog $i
+done
+
+rm -rf $RPM_BUILD_ROOT%{_datadir}/locale/
+rm -f $RPM_BUILD_ROOT%{_infodir}/bfd*
+rm -f $RPM_BUILD_ROOT%{_infodir}/standard*
+rm -f $RPM_BUILD_ROOT%{_infodir}/configure*
+rm -rf $RPM_BUILD_ROOT%{_includedir}/*.h
+rm -rf $RPM_BUILD_ROOT/%{_libdir}/lib{bfd*,opcodes*,iberty*,ctf*}
+
+cp -p %{SOURCE2} $RPM_BUILD_ROOT%{_mandir}/man1/gstack.1
+ln -s gstack.1 $RPM_BUILD_ROOT%{_mandir}/man1/pstack.1
+ln -s gstack $RPM_BUILD_ROOT%{_bindir}/pstack
+
+(cd $RPM_BUILD_ROOT%{_datadir}/gdb/syscalls
+rm -f mips*.xml
+rm -f sparc*.xml
+%ifnarch x86_64
+rm -f amd64-linux.xml
+%endif
+%ifnarch %{ix86} x86_64
+rm -f i386-linux.xml
+%endif
+)
+
+rm -f $RPM_BUILD_ROOT%{_infodir}/gdbint*
+rm -f $RPM_BUILD_ROOT%{_infodir}/stabs*
+rm -f $RPM_BUILD_ROOT%{_infodir}/dir
+rm -f $RPM_BUILD_ROOT%{_datadir}/gdb/system-gdbinit/elinos.py
+rm -f $RPM_BUILD_ROOT%{_datadir}/gdb/system-gdbinit/wrs-linux.py
+rmdir $RPM_BUILD_ROOT%{_datadir}/gdb/system-gdbinit
+rm -f $RPM_BUILD_ROOT%{_datadir}/gdb/python/gdb/FrameWrapper.py
+rm -f $RPM_BUILD_ROOT%{_datadir}/gdb/python/gdb/backtrace.py
+rm -f $RPM_BUILD_ROOT%{_datadir}/gdb/python/gdb/command/backtrace.py
+
+}
+
