dea07cf24623cb45ad0a7b499c7848f57643619e
邱堂珂 <q_qiutangke@163.com>
2022-10-22 09:20:32 +0000

    cifs
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

---
+++ sample2/cifs-utils/phase.sh
@@ -0,0 +1,25 @@
+#!/usr/bash
+
+prep() {
+%autosetup -n %{name}-%{version} -p1
+
+}
+
+build() {
+autoreconf -vif
+%configure  --prefix=/usr --with-pamdir=%{_libdir}/security  ROOTSBINDIR=%{_sbindir}
+make %{?_smp_mflags}
+
+}
+
+install() {
+rm -rf %{buildroot}
+%make_install
+mkdir -p %{buildroot}%{_sysconfdir}/%{name}
+ln -s %{_libdir}/%{name}/idmapwb.so %{buildroot}%{_sysconfdir}/%{name}/idmap-plugin
+mkdir -p %{buildroot}%{_sysconfdir}/request-key.d
+install -m 644 contrib/request-key.d/cifs.idmap.conf %{buildroot}%{_sysconfdir}/request-key.d
+install -m 644 contrib/request-key.d/cifs.spnego.conf %{buildroot}%{_sysconfdir}/request-key.d
+
+}
+
