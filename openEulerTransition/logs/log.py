fa0076b2458c55707fd6f5d78b4186288cf550ee
邱堂珂 <q_qiutangke@163.com>
2023-02-23 06:38:04 +0000

    update openEulerTransition/logs/log.py.
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

--- openEulerTransition/logs/log.py
+++ openEulerTransition/logs/log.py
@@ -6,6 +6,8 @@
 """
 import logging
 import os
+import time
+from logging import handlers
 
 print_color = {"black": 30, "red": 31, "green": 32, "yellow": 33,
                "blue": 34, "purple": 35, "darkGreen": 36, "white": 37}
@@ -17,10 +19,8 @@ ERROR = logging.ERROR
 CRITICAL = logging.CRITICAL
 
 log_level = DEBUG
-
 if os.getenv('CI_BUILD_DEBUG') != 'True':
     log_level = INFO
-
 
 class LogInfo:
     def __init__(self, ):
@@ -36,7 +36,7 @@ class LogInfo:
 
 class Logger(object):
     def __init__(self, name, clevel=log_level,
-                 log_file_path=None, Flevel=log_level):
+                 log_file_path=None, Flevel=log_level, only_file=False):
         fmt = logging.Formatter("%(asctime)s - [%(levelname)s] : %(message)s")
 
         ch = logging.StreamHandler()
@@ -48,10 +48,15 @@ class Logger(object):
         self.logger.addHandler(ch)
 
         if log_file_path:
-            fh = logging.FileHandler(log_file_path)
+            self.logger = logging.getLogger(name)
+            if only_file:
+                fh = logging.FileHandler(filename=log_file_path, encoding="utf-8")
+            else:
+                fh = handlers.TimedRotatingFileHandler(filename=log_file_path, when="D")
             fh.setLevel(Flevel)
             fh.setFormatter(fmt)
             self.logger.addHandler(fh)
+            self.logger.removeHandler(ch)
 
     def debug(self, message):
         self.logger.debug(message)
@@ -69,6 +74,26 @@ class Logger(object):
         self.logger.critical(message)
 
 
-logger = Logger("build")
+class LoggerSample:
+    def __init__(self, name, clevel=log_level,
+                 log_file_path=None, Flevel=log_level):
+        self.logger_sample = Logger(name, log_file_path=log_file_path, clevel=clevel, Flevel=Flevel, only_file=True)
+        self.logger_sample0 = Logger(name, log_file_path=None, clevel=ERROR, Flevel=ERROR)
+
+    def debug(self, message):
+        self.logger_sample.debug(message)
+
+    def info(self, message):
+        self.logger_sample.info(message)
+
+    def warn(self, message):
+        self.logger_sample.warn(message)
+
+    def error(self, message):
+        self.logger_sample0.error(message)
+
+
+logger = LoggerSample("build", log_file_path="openEulerTransition_{0}.log".format(
+    str(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()))))
 # Unified log format，change WARNING to WARN
 logging.addLevelName(WARN, 'WARN')
