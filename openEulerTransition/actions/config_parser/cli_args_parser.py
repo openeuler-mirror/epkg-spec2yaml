d74a91e8042186d5234c8abf63b730774db24dc9
邱堂珂 <q_qiutangke@163.com>
2023-04-07 07:44:35 +0000

    update openEulerTransition/actions/config_parser/cli_args_parser.py.
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

--- openEulerTransition/actions/config_parser/cli_args_parser.py
+++ openEulerTransition/actions/config_parser/cli_args_parser.py
@@ -30,12 +30,12 @@ class ArgsParser:
         self.type = _type
 
         cmd_args = {
-            'trans': '_trans_file',
             'conf': '_conf_file',
             'parse': '_parse_file',
             'shell': '_shell_script',
             'python': '_python_script',
             'target': '_target_type',
+            'log': '_log_file',
         }
 
         if self.type == 'openEuler':
@@ -46,13 +46,6 @@ class ArgsParser:
             logger.error("%s\n            Reason:%s\n        EXCEPTION_RESPONSE:%s\n" % (
                 EXCEPTION_CODE[600], 'Invalid Command Line', EXCEPTION_RESPONSE[1]))
             raise Exception(EXCEPTION_CODE[600], 'Invalid Command Line')
-
-    def _trans_file(self):
-        self._parser.add_option(
-            '--trans', '-t',
-            dest="yaml_file",
-            type="string", default=None,
-            help='Trans file.')
 
     def _parse_file(self):
         self._parser.add_option(
@@ -77,7 +70,7 @@ class ArgsParser:
 
     def _target_type(self):
         self._parser.add_option(
-            '--target', '-g',
+            '--target', '-t',
             dest="target_type",
             type="string", default="spec",
             help='File type.')
@@ -88,6 +81,14 @@ class ArgsParser:
             dest="conf_file",
             type="string", default=None,
             help='Conf file.'
+        )
+
+    def _log_file(self):
+        self._parser.add_option(
+            '--logpath', '-l',
+            dest="log_path",
+            type="string", default="",
+            help='Log path.'
         )
 
     def parser(self):
