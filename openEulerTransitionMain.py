3957d65995bfb78bf86deecb1bc85857c40167a6
邱堂珂 <q_qiutangke@163.com>
2023-04-07 08:14:50 +0000

    update openEulerTransitionMain.py.
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

--- openEulerTransitionMain.py
+++ openEulerTransitionMain.py
@@ -2,6 +2,7 @@
 # -*- encoding: utf-8 -*-
 import os
 import sys
+import time
 from openEulerTransition.logs.log import logger
 from openEulerTransition.actions.config_parser.cli_args_parser import BuildArgsParser
 from openEulerTransition.actions.generation.spec_file import CreateSPEC
@@ -10,15 +11,15 @@ from openEulerTransition.actions.generation.yaml_file import CreateYAML
 
 def main():
     (options, _) = BuildArgsParser('openEuler', sys.argv[1:]).parser()
+    log_path = options.log_path
+    if not os.path.exists(log_path):
+        log_path = ""
+    if log_path != "":
+        log_file_name = "openEulerTransition_{0}.log".format(
+            str(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())))
+        logger.log_file_path = os.path.join(log_path, log_file_name)
+        logger.change_path()
     if options.yaml_file:
-        log_path = options.log_path
-        if not os.path.exists(log_path):
-            log_path = ""
-        if log_path != "":
-            log_file_name = "openEulerTransition_{0}.log".format(
-                str(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())))
-            logger.log_file_path = os.path.join(log_path, log_file_name)
-            logger.change_path()
         if options.target_type == "spec":
             CreateSPEC(options.yaml_file, conf_path=options.conf_file, shell_path=options.shell_script,
                        python_path=options.python_script).transition()
