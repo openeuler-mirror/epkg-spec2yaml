0c0559e62c73358b448f2b0343473b65577f142f
邱堂珂 <q_qiutangke@163.com>
2023-04-07 07:27:01 +0000

    update openEulerTransitionMain.py.
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

--- openEulerTransitionMain.py
+++ openEulerTransitionMain.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
+import os
 import sys
-from openEulerTransition.logs.log import logger
+import time
 from openEulerTransition.actions.config_parser.cli_args_parser import BuildArgsParser
-from openEulerTransition.actions.generation.spec_file import CreateSPEC
-from openEulerTransition.actions.generation.yaml_file import CreateYAML
+from openEulerTransition.logs.log import logger
+from openEulerTransition.actions.writer.yaml_writer import YamlWriter
+from openEulerTransition.actions.utils.file_operate import check_spec_file
 
 
 def main():
     (options, _) = BuildArgsParser('openEuler', sys.argv[1:]).parser()
-    if options.yaml_file:
-        if options.target_type == "spec":
-            CreateSPEC(options.yaml_file, conf_path=options.conf_file, shell_path=options.shell_script,
-                       python_path=options.python_script).transition()
-        else:
-            logger.error("There is no other target type to solve than spec")
-    elif options.spec_file:
-        CreateYAML(options.spec_file).transition()
+    log_path = options.log_path
+    if not os.path.exists(log_path):
+        log_path = ""
+    if log_path != "":
+        log_file_name = "openEulerTransition_{0}.log".format(
+            str(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())))
+        logger.log_file_path = os.path.join(log_path, log_file_name)
+        logger.change_path()
+    if not check_spec_file(options.spec_file):
+        logger.error("Cannot find valid spec file, file path is " + options.spec_file)
+        return
+    yaml_writer = YamlWriter(options.spec_file)
+    yaml_writer.parse()
 
 
 if __name__ == '__main__':
