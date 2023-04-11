5abb98fae791866f38327767fa3669bd64c82e2a
邱堂珂 <q_qiutangke@163.com>
2023-04-11 08:48:01 +0000

    去掉不需要的配置
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

--- openEulerTransition/configure/yaml_config.py
+++ openEulerTransition/configure/yaml_config.py
@@ -1,20 +1,12 @@
-import os.path
-
-YAML_CHAIN_SHELL_KEYWORDS = ["install", "prep", "build", "pre", "preun", "post", "postun", "check", "petrans",
-                             "posttrans", "clean"]
-YAML_LINES_KEYWORDS = ["meta.description"]
 NEED_QUOTATION_KEYWORDS = ['Provides', 'Obsoletes', 'BuildRequires', 'Requires', 'Requires(postun)', 'BuildRoot',
                            'Requires(pre)', 'PreRequires', 'PreReq', 'Prereq', 'Requires(preun)', 'Provides',
-                           'Obsoletes', 'Conflicts', 'BuildConflicts', 'ExclusiveArch', 'Macros', 'OrderWithRequires',
+                           'Obsoletes', 'Conflicts', 'BuildConflicts', 'ExclusiveArch', 'OrderWithRequires',
                            'Requires(post)', 'Requires(posttrans)', 'Version']
 MAY_QUOTATION_KEYWORDS = ['Version', 'Sources', 'Description', 'Summary', 'Patches', 'Files', 'FilesInput',
                           'Epoch', 'Release', 'License', 'Recommends', 'Suggests', 'ExcludeArch', "Name",
-                          "Supplements", 'URL', 'Prefix', 'IncludeSource', "FilesJudgement"]
-
-CONF_CONTENT = "export CC=/usr/bin/gcc" + os.linesep + "export CXX=/usr/bin/g++" + os.linesep + \
-               "export BUILD_SYS=`uname -m`-openeuler-`uname -s`-gnu" + os.linesep + \
-               "export HOST_SYS=`uname -m`-openeuler-`uname -s`-gnu"
-
-REQUIRES_REPLACE = {"requiresPost:": "requires(post):", "requiresPostUn:": "requires(postun):",
-                    "requiresPre:": "requires(pre):", "requiresPreUn:": "requires(preun):",
-                    "requiresPretrans:": "requires(pretrans):", "requiresPosttrans:": "requires(posttrans):"}
+                          "Supplements", 'URL', 'Prefix', 'IncludeSource', 'rpmMacros', "FilesJudgement", "rpmGlobal"]
+LIST_KEY_REPLACE = {"BuildRequires": "buildRequires", "Requires": "requires", "Provides": "provides",
+                    "Obsoletes": "obsoletes", "Requires(post)": "requiresPost",
+                    "Requires(postun)": "requiresPostUn", "Requires(pre)": "requiresPre",
+                    "Requires(preun)": "requiresPreun", "Requires(pretrans)": "requiresPretrans",
+                    "Requires(posttrans)": "requiresPosttrans"}
