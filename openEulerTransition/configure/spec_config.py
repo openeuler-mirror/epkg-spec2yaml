4793370378bea4fa1c35f66cf008ccadfcacd3be
邱堂珂 <q_qiutangke@163.com>
2023-04-18 11:31:06 +0000

    update openEulerTransition/configure/spec_config.py.
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

--- openEulerTransition/configure/spec_config.py
+++ openEulerTransition/configure/spec_config.py
@@ -36,6 +36,7 @@ SINGLES = ('Summary',
            'Prefix',
            'License',
            "Release",
+           "Recommends",
            "ExclusiveArch")
 REQUIRES = ('BuildRequires',
             'Requires',
@@ -99,42 +100,6 @@ ORDER_ENTRIES = ['Macros',
                  'rpmGlobal',
                  'useFlag'
                  ]
-# must have keys for 'main' package
-MUSTHAVE = {'Release': '1',
-            }
-
-TAB = '    '  # 4space, instead of Tab
-KEY_SYS = {
-            'BuildRequires': 'BuildRequires',
-            'description': 'Description',
-            'Requires(post)': 'RequiresPost',
-            'Requires(postun)': 'RequiresPostUn',
-            'Requires(posttrans)': 'RequiresPostTrans',
-            'Requires(pre)': 'RequiresPre',
-            'PreRequires': 'RequiresPre',
-            'PreReq': 'RequiresPre',
-            'Prereq': 'RequiresPre',
-            'Requires(preun)': 'RequiresPreUn',
-            'Requires(pretrans)': 'RequiresPreTrans',
-            'Url': 'URL',
-            'install': 'Install',
-            'build': 'Build',
-            'files': 'Files',
-            'clean': 'Clean',
-            'pre': 'Pre',
-            'preun': 'Preun',
-            'post': 'Post',
-            'postun': 'Postun',
-            'pretrans': 'Pretrans',
-            'posttrans': 'Posttrans',
-            'transfiletrigger\w+': 'transfileTrigger\w+',
-            'check': 'Check',
-            'prep': 'Prep',
-            'include': 'IncludeSource',
-            'Autoreq': 'AutoReq',
-            'Autoprov': 'AutoProv',
-            'Autoreqprov': 'AutoReqProv',
-        }
 
 SPEC_EXTRA_KEYS = {
         # key -> ( nkey, nsubkey)
@@ -150,3 +115,40 @@ SPEC_EXTRA_KEYS = {
         'PostMakeExtras': ('build', 'post'),
         'PreMakeExtras': ('build', 'pre'),
     }
+
+RPM_GLOBAL_MACROS = {
+    "GNAT_arches": "%{GPRbuild_arches} %{generic_arches}",
+    "GPRbuild_arches": "%{generic_arches}",
+    "__7zip": "/usr/bin/7za",
+    "___build_args": "-e",
+    "___build_cmd": "%{?_sudo:%{_sudo} }%{?_remsh:%{_remsh} %{_remhost} }%{?_remsudo:%{_remsudo} }%{?_remchroot:"
+                    "%{_remchroot} %{_remroot} }%{___build_shell} %{___build_args}",
+    "___build_post": "RPM_EC=$?"
+                     "for pid in $(jobs -p); do kill -9 ${pid} || continue; done"
+                     "exit ${RPM_EC}"
+                     "%{nil}",
+    "___build_shell": "%{?_buildshell:%{_buildshell}}%{!?_buildshell:/bin/sh}",
+    "___build_template": "#!%{___build_shell}"
+                         "%{___build_pre}"
+                         "%{nil}",
+    "__apply_patch(qp:m:)": "%{lua:"
+                            "local file = rpm.expand(\"%{1}\")"
+                            "local num = rpm.expand(\"%{2}\")"
+                            "if posix.access(file, "r") then"
+                            "    local options = rpm.expand(\"%{-q} %{-p:-p%{-p*}} %{-m:-m%{-m*}}\")"
+                            "    local scm_apply = rpm.expand(\"%__scm_apply_%{__scm}\")"
+                            "    print(rpm.expand(\"%{uncompress:\"..file..\"} | \"..scm_apply.." "..options.."  "..file.." "..num..\"\n\"))"
+                            "else"
+                            "    print(\"echo 'Cannot read \"..file..\"'; exit 1;\"..\"\n\")"
+                            "end}",
+    "__ar": "ar",
+    "__arch_install_post": "/usr/lib/rpm/check-buildroot",
+    "nil": "%{!?nil}",
+    "nim_arches": "%{generic_arches}",
+    "nodejs_arches": "%{generic_arches}",
+    "ocaml_natdynlink": "%{generic_arches}",
+    "ocaml_native_compiler": "%{generic_arches}",
+    "ocaml_native_profiling": "%{generic_arches}",
+    "openEuler": "2",
+    "optflags": "%{__global_compiler_flags} -m64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection",
+}
