f3d23e68293658f91678144cb1fca49aad037b18
邱堂珂 <q_qiutangke@163.com>
2023-05-27 04:31:03 +0000

    update openEulerTransition/actions/writer/yaml_writer.py.
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

--- openEulerTransition/actions/writer/yaml_writer.py
+++ openEulerTransition/actions/writer/yaml_writer.py
@@ -1419,10 +1419,10 @@ class SpecParser(object):
                         if not whole_name else sub_member_name.strip()
                     for member_key, member_value in sub_member_dict.items():
                         if member_key == "files":
-                            if "%if" in sub_member_name:
-                                sub_files_judgement = " " + change_judgement_grammar(sub_member_name, self.rpm_global, cut_judge=True, macros_text=self.macros)
-                            else:
-                                sub_files_judgement = ""
+                            # if "%if" in sub_member_name:
+                            #     sub_files_judgement = " " + change_judgement_grammar(sub_member_name, self.rpm_global, cut_judge=True, macros_text=self.macros)
+                            # else:
+                            sub_files_judgement = ""
                             self.files["subpackage." + sub_file_name + ".files" + sub_files_judgement] = member_value
                             del target_data["SubPackages"][sub_member_name]["files"]
                         if member_key == "Summary" and len(member_value) == 1 and member_value[0].startswith("`"):
