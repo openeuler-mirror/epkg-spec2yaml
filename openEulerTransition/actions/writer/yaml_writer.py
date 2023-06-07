af897070dc7258dd0cf9c466931ee021d112662f
“qiutangke” <q_qiutangke@163.com>
2023-06-07 16:34:33 +0800

    bug

--- openEulerTransition/actions/writer/yaml_writer.py
+++ openEulerTransition/actions/writer/yaml_writer.py
@@ -505,8 +505,6 @@ class YamlWriter:
 def pre_treatment(content):
     if "%%" in content:
         content = content.replace("%%", "\\%\\%").replace("\\%%", "\\%\\%")
-    if "\t" in content:
-        content = content.replace("\t", "")
     for system_macros in RPM_SYSTEM_MACROS:
         if os.linesep + system_macros in content:
             content = content.replace(os.linesep + system_macros,
