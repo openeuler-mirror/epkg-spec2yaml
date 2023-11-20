2d589c449dcfcb6e769814e8448539d3fc4f7827
qiu-tangke <q_qiutangke@163.com>
2023-11-20 19:36:34 +0800

    change struct of suggests key

--- openEulerTransition/configure/yaml_config.py
+++ openEulerTransition/configure/yaml_config.py
@@ -9,7 +9,8 @@ LIST_KEY_REPLACE = {"BuildRequires": "buildRequires", "Requires": "requires", "P
                     "Obsoletes": "obsoletes", "Requires(post)": "requiresPost", "Conflicts": "conflicts",
                     "Requires(postun)": "requiresPostUn", "Requires(pre)": "requiresPre",
                     "Requires(preun)": "requiresPreun", "Requires(pretrans)": "requiresPretrans",
-                    "Requires(posttrans)": "requiresPosttrans", "Recommends": "recommends"}
+                    "Requires(posttrans)": "requiresPosttrans", "Recommends": "recommends",
+                    "Suggests": "suggests"}
 # must have keys for 'main' package
 MUSTHAVE = {'Release': '1'}
 
